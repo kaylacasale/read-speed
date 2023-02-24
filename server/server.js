02cd74cc19cbe279b787aec1588013cb5c309f6c
Kayla Casale <kayla.casale@gmail.com>
2023-02-24 15:43:56 -0800

    ignore node_modules

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
@@ -32,14 +32,14 @@ app.get('/', (req, res) => {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-  };
-  
+};
+
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
